12740be54bbd969f11dbb382e59b6f493d6d9de7
Mateo Ormeño <mateoormeno@MacBook-Pro-de-Mateo.local>
2022-06-21 23:39:54 -0400

    views

--- src/components/LandingForm.tsx
+++ src/components/LandingForm.tsx
@@ -1,106 +1,150 @@
-import React, {useReducer, useState} from "react";
-import { Image, Header, LandingFormContainer, PageContainer, InstructionContainer, Instruction, FormContainer, FormColumn, DateContainer, DateLabel, FormRow, Optional, StyledButton } from "./styles/LandingForm.styled";
+import React, { useReducer, useState } from "react";
+import {
+  Image,
+  Header,
+  LandingFormContainer,
+  PageContainer,
+  InstructionContainer,
+  Instruction,
+  FormContainer,
+  FormColumn,
+  DateContainer,
+  DateLabel,
+  FormRow,
+  Optional,
+  StyledButton,
+} from "./styles/LandingForm.styled";
 import { TextInput, SelectInput } from "./styles/Inputs.styled";
-import {default as ciudades} from '../ciudades_codigos.json';
-import Logo from '../optriplogo.png';
-import {DateRangeInput, DateSingleInput, Datepicker} from '@datepicker-react/styled';
+import { default as ciudades } from "../ciudades_codigos.json";
+import Logo from "../optriplogo.png";
+import {
+  DateRangeInput,
+  DateSingleInput,
+  Datepicker,
+} from "@datepicker-react/styled";
 import { ThemeProvider } from "styled-components";
 import FlightsDisplay from "./FlightsDisplay";
 import { Link } from "react-router-dom";
 const initialState = {
-    date: new Date(),
-    showDatepicker: false,
+  date: new Date(),
+  showDatepicker: false,
+};
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case "focusChange":
+      return { ...state, showDatepicker: action.payload };
+    case "dateChange":
+      return action.payload;
+    default:
+      throw new Error();
   }
-  
-  function reducer(state : any, action : any ) {
-    switch (action.type) {
-      case 'focusChange':
-        return {...state, showDatepicker: action.payload}
-      case 'dateChange':
-        return action.payload
-      default:
-        throw new Error()
-    }
-  }
+}
 
 export default function LandingForm() {
-    const [state, dispatch] = useReducer(reducer, initialState)
-    const [From, setFrom] = useState('SCL');
-    const [To, setTo] = useState('SCL');
-    const [SearchDate, setSearchDate] = useState(new Date());
-    const [MaxTime, setMaxTime] = useState('');
-    const [MaxCost, setMaxCost] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [From, setFrom] = useState("SCL");
+  const [To, setTo] = useState("SCL");
+  const [SearchDate, setSearchDate] = useState(new Date());
+  const [MaxTime, setMaxTime] = useState("");
+  const [MaxCost, setMaxCost] = useState("");
 
-    return (
-        <>
-        {console.log(From)}
-        {console.log(To)}
-        {console.log(state.date)}
-        {console.log(MaxTime)}
-        {console.log(MaxCost)}
-        <PageContainer>
-       
-            <Image src={Logo} />
-            <Header>OPTRIP</Header>
-            <LandingFormContainer>
-                <InstructionContainer>
-                    <Instruction>Ingresa los parámetros solicitados para realizar la búsqueda: </Instruction>
-                </InstructionContainer>
-                <FormContainer>
-                    <FormRow>
-                        <FormColumn>
-                            <SelectInput required id={'origen'} label={'Origen'} onChange={(input) => setFrom(input.target.value) }>
-                            {ciudades.map((ciudad) => {
-                                return <option key={ciudad.codigo}>{ciudad.codigo}</option>;
-                            })}
-                            </SelectInput>
-                        </FormColumn>
-                        <FormColumn>
-                            <SelectInput required id={'origen'} label={'Destino'} onChange={(input) => setTo(input.target.value) }>
-                            {ciudades.map((ciudad) => {
-                                return <option key={ciudad.codigo}>{ciudad.codigo}</option>;
-                            })}
-                            </SelectInput>
-                        </FormColumn>
-                        <FormColumn>
-                            <DateContainer>
-                                <DateLabel>Fecha</DateLabel>
-                                <DateSingleInput
-                                onDateChange={data => dispatch({type: 'dateChange', payload: data})}
-                                onFocusChange={focusedInput => dispatch({type: 'focusChange', payload: focusedInput})}
-                                date={state.date} // Date or null
-                                showDatepicker={state.showDatepicker} // Boolean
-                                />
-                            </DateContainer>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <InstructionContainer>
-                            <Optional>Valores opcionales, "Tiempo" en horas y "Presupuesto" en CLP</Optional>
-                        </InstructionContainer>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <TextInput type="number" required id="Tiempo Max" label="Tiempo Max" onChange={(input) => setMaxTime(input.target.value) }/>
-                        </FormColumn>
-                        <FormColumn>
-                            <TextInput type="number" required id="Presupuesto" label="Presupuesto" onChange={(input) => setMaxCost(input.target.value) }/>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                    <FormColumn>
-                            
-                            <Link to="/flights" state={{From:From, To:To, Date:state.date}}>
-                            <StyledButton>
-                                Buscar
-                            </StyledButton>
-                                </Link>
-                            
-                            </FormColumn>
-                    </FormRow>
-                </FormContainer>
-            </LandingFormContainer>
-        </PageContainer>
-        </>
-    )
+  return (
+    <>
+      {console.log(From)}
+      {console.log(To)}
+      {console.log(state.date)}
+      {console.log(MaxTime)}
+      {console.log(MaxCost)}
+      <PageContainer>
+        <Image src={Logo} />
+        <Header>OPTRIP</Header>
+        <LandingFormContainer>
+          <InstructionContainer>
+            <Instruction>
+              Ingresa los parámetros solicitados para realizar la búsqueda:{" "}
+            </Instruction>
+          </InstructionContainer>
+          <FormContainer>
+            <FormRow>
+              <FormColumn>
+                <SelectInput
+                  required
+                  id={"origen"}
+                  label={"Origen"}
+                  onChange={(input) => setFrom(input.target.value)}
+                >
+                  {ciudades.map((ciudad) => {
+                    return <option key={ciudad.codigo}>{ciudad.codigo}</option>;
+                  })}
+                </SelectInput>
+              </FormColumn>
+              <FormColumn>
+                <SelectInput
+                  required
+                  id={"origen"}
+                  label={"Destino"}
+                  onChange={(input) => setTo(input.target.value)}
+                >
+                  {ciudades.map((ciudad) => {
+                    return <option key={ciudad.codigo}>{ciudad.codigo}</option>;
+                  })}
+                </SelectInput>
+              </FormColumn>
+              <FormColumn>
+                <DateContainer>
+                  <DateLabel>Fecha</DateLabel>
+                  <DateSingleInput
+                    onDateChange={(data) =>
+                      dispatch({ type: "dateChange", payload: data })
+                    }
+                    onFocusChange={(focusedInput) =>
+                      dispatch({ type: "focusChange", payload: focusedInput })
+                    }
+                    date={state.date} // Date or null
+                    showDatepicker={state.showDatepicker} // Boolean
+                  />
+                </DateContainer>
+              </FormColumn>
+            </FormRow>
+            <FormRow>
+              <InstructionContainer>
+                <Optional>
+                  Valores opcionales, "Tiempo" en horas y "Presupuesto" en CLP
+                </Optional>
+              </InstructionContainer>
+            </FormRow>
+            <FormRow>
+              <FormColumn>
+                <TextInput
+                  type="number"
+                  required
+                  id="Tiempo Max"
+                  label="Tiempo Max"
+                  onChange={(input) => setMaxTime(input.target.value)}
+                />
+              </FormColumn>
+              <FormColumn>
+                <TextInput
+                  type="number"
+                  required
+                  id="Presupuesto"
+                  label="Presupuesto"
+                  onChange={(input) => setMaxCost(input.target.value)}
+                />
+              </FormColumn>
+            </FormRow>
+            <FormRow>
+              <Link
+                to="/flights"
+                state={{ From: From, To: To, Date: state.date }}
+              >
+                <StyledButton>Buscar</StyledButton>
+              </Link>
+            </FormRow>
+          </FormContainer>
+        </LandingFormContainer>
+      </PageContainer>
+    </>
+  );
 }
--- src/components/styles/LandingForm.styled.ts
+++ src/components/styles/LandingForm.styled.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
-
+import background_image from "../../wallpaper.jpg";
 
 export const PageContainer = styled.div`
-    height: fit-content;
-    display: flex;
-    /* border: 3px solid #283346; */
-    /* border-radius: 10px; */
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  /* border: 3px solid #283346; */
+  /* border-radius: 10px; */
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  background: url(${background_image});
+  background-size: 100vw 100vh;
+`;
 
 /* Unused */
 // export const HeaderImageContainer = styled.div`
@@ -32,97 +35,97 @@ export const PageContainer = styled.div`
 //     align-content: center;
 // `
 export const Header = styled.h1`
-    text-align: center;
-    font-size: 60px;
-    color: teal;
-    &:hover {
-        opacity: 70%;
-    }
-`
+  text-align: center;
+  font-size: 60px;
+  color: teal;
+  &:hover {
+    opacity: 70%;
+  }
+`;
 export const Image = styled.img`
-    flex: 1;
-    max-width: 10vw;
-    opacity: 90%;
-`
+  flex: 1;
+  max-width: 10vw;
+  opacity: 90%;
+`;
 
 export const LandingFormContainer = styled.div`
-    padding: 4vh 1vw;
-    margin: 2vh 1vw;
-    height: fit-content;
-    flex: 1;
-    display: flex;
-    border: 3px solid #283346;
-    border-radius: 10px;
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-`
+  padding: 4vh 1vw;
+  margin: 2vh 1vw;
+  height: fit-content;
+  flex: 1;
+  display: flex;
+  border: 3px solid #283346;
+  border-radius: 10px;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`;
 
 export const InstructionContainer = styled.div`
-    flex: 1;
-    display: flex;
-    max-width: 100%;
-    max-height: 100%;
-    justify-content: center;
-    justify-items: center;
-    align-items: center;
-    align-content: center;
-`
+  flex: 1;
+  display: flex;
+  max-width: 100%;
+  max-height: 100%;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+`;
 
 export const Instruction = styled.h1`
-    text-align: center;
-    font-size: 34px;
-`
+  text-align: center;
+  font-size: 34px;
+`;
 
 export const Optional = styled.h2`
-    text-align: center;
-    font-size: 20px;
-    opacity: 70%;
-`
+  text-align: center;
+  font-size: 20px;
+  opacity: 70%;
+`;
 
 export const FormContainer = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    margin: 0 5vw;
-    max-width: 100%;
-`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin: 0 5vw;
+  max-width: 100%;
+`;
 
 export const FormRow = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: row;
-    margin: 0 5vw;
-    max-width: 100%;
-`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  margin: 0 5vw;
+  max-width: 100%;
+`;
 
 export const FormColumn = styled.div`
-    margin: 2% 0;
-    flex: 1;
-    display: flex;
-    justify-content: 'center';
-    justify-items:  'center';
-`
+  margin: 2% 0;
+  flex: 1;
+  display: flex;
+  justify-content: "center";
+  justify-items: "center";
+`;
 
 export const DateContainer = styled.div`
-    display: flex;
-    transform: translate(0% , 25%);
-    z-index: 2;
-`
+  display: flex;
+  transform: translate(0%, 25%);
+  z-index: 2;
+`;
 export const DateLabel = styled.h4`
-    transform: translate(100%,-220%);
-    font-size: 20px;
-    font-weight: bold;
-    color: #283346;
-`
+  transform: translate(100%, -220%);
+  font-size: 20px;
+  font-weight: bold;
+  color: #283346;
+`;
 export const StyledButton = styled.button`
-display: inline-block;
-color: black;
-font-size: 1em;
-margin: 1em;
-padding: 0.25em 1em;
-border: 2px solid blakc;
-border-radius: 3px;
-display: block;
-background:teal
-`
+  display: inline-block;
+  color: black;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid blakc;
+  border-radius: 3px;
+  display: block;
+  background: teal;
+`;
